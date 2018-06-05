11d630dba5ce9f68303128d879a30dc283d0fdbf
unknown <asfd;lkj>
2018-06-05 14:19:04 -0400

    Changed indentation and made function variables lowercase

--- scripts/addWebsite.py
+++ scripts/addWebsite.py
@@ -1,29 +1,35 @@
 import urllib
 import hashlib
-def openSite(str_url):
-        return urllib.urlopen(str_url)
+
+
+def opensite(str_url):
+    return urllib.urlopen(str_url)
+
 
 def main():
-        print "Enter a website to monitor for updates: "
-        site = raw_input()
-        text = openSite(site)
-        writeToFile(text, site)
+    print "Enter a website to monitor for updates: "
+    site = raw_input()
+    text = openSite(site)
+    writefile(text, site)
 
-def writeToFile(text, site):
-        m = hashlib.md5()
-        for line in text:
-                m.update(line)
-        fileName = "list.txt"
-        file = open(fileName, "a+")
-        lines = file.readlines()
-        for i in range(0, len(lines)):
-                if lines[i][0:len(lines[i]) - 1] == site + "\n":
-                        print "That website is already being monitored."
-                        if lines[i + 1] != m.hexdigest():
-                                print "But it has changed the last time we read it!"
-                                return
-        file.write(site + "\n")
-        file.write(m.hexdigest() + "\n")
-        file.close()
+
+def writefile(text, site):
+    m = hashlib.md5()
+    for line in text:
+        m.update(line)
+        filename = "list.txt"
+        myfile = open(filename, "a+")
+        lines = myfile.readlines()
+        for i in range(0, len(lines)):  # type: int
+            if lines[i][0:len(lines[i]) - 1] == site + "\n":
+                print "That website is already being monitored."
+                if lines[i + 1] != m.hexdigest():
+                    print "But it has changed the last time we read it!"
+                    return
+        myfile.write(site + "\n")
+        myfile.write(m.hexdigest() + "\n")
+        myfile.close()
+
+
 if __name__ == "__main__":
-        main()
+    main()
--- scripts/scanChanges.py
+++ scripts/scanChanges.py
@@ -1,11 +1,9 @@
-import time
 import urllib
 import hashlib
-def updateSiteList():
-    #every once in a while
-    file = open("list.txt", "r")
+def updatesitelist():
+    myfile = open("list.txt", "r")
     changed = []
-    lines = file.readlines()
+    lines = myfile.readlines()
     for i in range(0, len(lines), 2):
         site = urllib.urlopen(lines[i])
         h = hashlib.md5()
@@ -14,11 +12,11 @@ def updateSiteList():
         if h.hexdigest().strip() != lines[i + 1].strip():
             lines[i + 1] = h.hexdigest() + "\n"
             changed.append(lines[i])
-    file.close()
-    file = open("list.txt", "w+")
+    myfile.close()
+    myfile = open("list.txt", "w+")
     if len(changed) > 0:
-            print "Found changes in the following webpages:"
-            for line in changed:
-                    print line
+        print "Found changes in the following webpages:"
+        for line in changed:
+                print line
     for line in lines:
-        file.write(line)
+        myfile.write(line)
