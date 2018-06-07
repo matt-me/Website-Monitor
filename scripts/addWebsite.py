f6a03de0261a6a6620c6557290f4194a3d325ab2
unknown <asfd;lkj>
2018-06-07 09:14:58 -0400

    Fixed indentation problems

--- scripts/addWebsite.py
+++ scripts/addWebsite.py
@@ -1,5 +1,7 @@
 import urllib
 import hashlib
+
+
 def opensite(str_url):
     return urllib.urlopen(str_url)
 
@@ -15,15 +17,15 @@ def addsite(text, site, path):
     m = hashlib.md5()
     for line in text:
         m.update(line)
-        filename = "data/list.txt"
-        myfile = open(path + filename, "a+")
-        lines = myfile.readlines()
-        for i in range(0, len(lines)):  # type: int
-            if lines[i][0:len(lines[i]) - 1] == site + "\n":
-                print "That website is already being monitored."
-                if lines[i + 1] != m.hexdigest():
-                    print "But it has changed the last time we read it!"
-                    return
+    filename = "data/list.txt"
+    myfile = open(path + filename, "a+")
+    lines = myfile.readlines()
+    for i in range(0, len(lines), 2):  # type: int
+        if lines[i] == site + "\n":
+            print "That website is already being monitored."
+            if lines[i + 1][0:len(lines[i + 1]) - 1] != m.hexdigest():
+                print "But it has changed the last time we read it!"
+            return
     myfile.write(site + "\n")
     myfile.write(m.hexdigest() + "\n")
     myfile.close()
